983724e972f9d359d0e62bebb7990332a93b4389
hianshul07 <hianshul07@gmail.com>
2024-02-13 22:10:48 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ListGroup from "./components/ListGroup"
+
+// import Message from "./Message"
+
+const App = () => {
+  return (
+    <div>
+      <ListGroup />
+    </div>
+  )
+}
+export default App
